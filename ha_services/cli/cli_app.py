9fa332a96efdfe0d8e9a247766b0d78ae5a77c78
JensDiemer <git@jensdiemer.de>
2023-05-07 15:02:59 +0200

    init

---
+++ ha_services/cli/cli_app.py
@@ -0,0 +1,72 @@
+"""
+    CLI for usage
+"""
+import logging
+import sys
+from pathlib import Path
+
+import rich_click as click
+from bx_py_utils.path import assert_is_file
+from rich import print  # noqa
+from rich_click import RichGroup
+
+import ha_services
+from ha_services import constants, __version__
+
+
+logger = logging.getLogger(__name__)
+
+
+PACKAGE_ROOT = Path(ha_services.__file__).parent.parent
+assert_is_file(PACKAGE_ROOT / 'pyproject.toml')
+
+OPTION_ARGS_DEFAULT_TRUE = dict(is_flag=True, show_default=True, default=True)
+OPTION_ARGS_DEFAULT_FALSE = dict(is_flag=True, show_default=True, default=False)
+ARGUMENT_EXISTING_DIR = dict(
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True, path_type=Path)
+)
+ARGUMENT_NOT_EXISTING_DIR = dict(
+    type=click.Path(
+        exists=False,
+        file_okay=False,
+        dir_okay=True,
+        readable=False,
+        writable=True,
+        path_type=Path,
+    )
+)
+ARGUMENT_EXISTING_FILE = dict(
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, path_type=Path)
+)
+
+
+class ClickGroup(RichGroup):  # FIXME: How to set the "info_name" easier?
+    def make_context(self, info_name, *args, **kwargs):
+        info_name = './cli.py'
+        return super().make_context(info_name, *args, **kwargs)
+
+
+@click.group(
+    cls=ClickGroup,
+    epilog=constants.CLI_EPILOG,
+)
+def cli():
+    pass
+
+
+@click.command()
+def version():
+    """Print version and exit"""
+    # Pseudo command, because the version always printed on every CLI call ;)
+    sys.exit(0)
+
+
+cli.add_command(version)
+
+
+def main():
+    print(f'[bold][green]ha_services[/green] v[cyan]{__version__}')
+
+    # Execute Click CLI:
+    cli.name = './cli.py'
+    cli()
