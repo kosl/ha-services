693491634151b78f6917e137788d0cf25e59e111
JensDiemer <git@jensdiemer.de>
2024-03-22 15:35:06 +0100

    Complete refactor mqtt4homeassistant module

--- ha_services/example.py
+++ ha_services/example.py
@@ -1,16 +1,20 @@
 import dataclasses
 import logging
 import os
+import random
 import resource
 import time
 
 from cli_base.systemd.data_classes import BaseSystemdServiceInfo, BaseSystemdServiceTemplateContext
+from paho.mqtt.client import Client
 from rich import print  # noqa
 
 import ha_services
-from ha_services.mqtt4homeassistant.converter import values2mqtt_payload
-from ha_services.mqtt4homeassistant.data_classes import HaValue, HaValues, MqttSettings
-from ha_services.mqtt4homeassistant.mqtt import HaMqttPublisher
+from ha_services.mqtt4homeassistant.components.sensor import Sensor
+from ha_services.mqtt4homeassistant.components.switch import Switch
+from ha_services.mqtt4homeassistant.data_classes import MqttSettings
+from ha_services.mqtt4homeassistant.device import MqttDevice
+from ha_services.mqtt4homeassistant.mqtt import get_connected_client
 
 
 logger = logging.getLogger(__name__)
@@ -66,56 +70,83 @@ def publish_forever(*, user_settings: DemoSettings, verbosity: int):
     """
     Publish "something" to MQTT server. It's just a DEMO ;)
     """
-    publisher = HaMqttPublisher(
-        settings=user_settings.mqtt,
-        verbosity=verbosity,
-        config_count=1,  # Send every time the config
+
+    device = MqttDevice(
+        name='ha-services Example',
+        uid='ha_services',
+        manufacturer='ha_services',
+        sw_version=ha_services.__version__,
     )
 
+    relay_delay = Sensor(
+        device=device,
+        name='Relay "delay"',
+        uid='relay_delay',
+        state_class='measurement',
+        unit_of_measurement='sec',
+    )
+
+    def relay_callback(*, client: Client, component: Switch, old_state: str, new_state: str):
+        logger.info(f'{component.name} state changed: {old_state!r} -> {new_state!r}')
+        delay = random.randrange(5)
+        logger.info(f'{delay=}')
+        relay_delay.set_state(delay)
+        relay_delay.publish_config_and_state(mqttc)
+        time.sleep(delay)
+
+        component.set_state(new_state)
+        component.publish_state(client)
+
+    relay = Switch(
+        device=device,
+        name='Virtual Relay',
+        uid='relay',
+        callback=relay_callback,
+    )
+    system_load_sensor = Sensor(
+        device=device,
+        name='System load 1min.',
+        uid='system_load',
+        state_class='measurement',
+        suggested_display_precision=2,
+    )
+    user_time_used_sensor = Sensor(
+        device=device,
+        name='Time in user mode (float seconds)',
+        uid='user_time_used',
+        state_class='measurement',
+        unit_of_measurement='sec',
+        suggested_display_precision=2,
+    )
+    system_time_used_sensor = Sensor(
+        device=device,
+        name='Time in system mode (float seconds)',
+        uid='system_time_used',
+        state_class='measurement',
+        unit_of_measurement='sec',
+        suggested_display_precision=2,
+    )
+
+    mqttc = get_connected_client(settings=user_settings.mqtt, verbosity=verbosity)
+    mqttc.loop_start()
+
+    relay.set_state(relay.OFF if random.randrange(2) else relay.ON)
+
     while True:
-        # Just collect something that we can send:
+        relay.publish_config_and_state(mqttc)
+
+        system_load_sensor.set_state(os.getloadavg()[0])
+        system_load_sensor.publish_config_and_state(mqttc)
+
         usage = resource.getrusage(resource.RUSAGE_SELF)
-        values = [
-            HaValue(
-                name='System load 1min.',
-                value=os.getloadavg()[0],
-                state_class='measurement',
-                unit='',
-            ),
-            HaValue(
-                name='Time in user mode (float seconds)',
-                value=usage.ru_utime,
-                state_class='measurement',
-                unit='sec',
-            ),
-            HaValue(
-                name='Time in system mode (float seconds)',
-                value=usage.ru_stime,
-                state_class='measurement',
-                unit='sec',
-            ),
-        ]
+        user_time_used_sensor.set_state(usage.ru_utime)
+        user_time_used_sensor.publish_config_and_state(mqttc)
 
-        # Collect information:
-        ha_values = HaValues(
-            device_name=user_settings.app.device_name,
-            values=values,
-        )
+        system_time_used_sensor.set_state(usage.ru_stime)
+        system_time_used_sensor.publish_config_and_state(mqttc)
 
-        # Create Payload:
-        ha_mqtt_payload = values2mqtt_payload(
-            values=ha_values,
-            name_prefix=user_settings.app.mqtt_payload_prefix,
-            device_extra_info={
-                'manufacturer': 'ha-services-demo',
-                'sw_version': ha_services.__version__,
-            },
-        )
-
-        # Send vial MQTT to HomeAssistant:
-        publisher.publish2homeassistant(ha_mqtt_payload=ha_mqtt_payload)
-
+        print('\n', flush=True)
         print('Wait', end='...')
         for i in range(10, 1, -1):
-            time.sleep(1)
-            print(i, end='...')
+            time.sleep(0.5)
+            print(i, end='...', flush=True)
